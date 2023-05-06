029dc927f1c70caac32be5c5f691264379555fa2
bsheese <bsheese@iwu.edu>
2023-05-05 23:13:48 -0500

    Update guppies.js

--- guppies.js
+++ guppies.js
@@ -1,48 +1,57 @@
 let money = 100;
+let round = 1;
 const betInput = document.getElementById('bet');
 const guessInput = document.getElementById('guess');
-const playButton = document.getElementById('play');
+const betButton = document.getElementById('betButton');
+const guessButton = document.getElementById('guessButton');
 const moneyDisplay = document.getElementById('money');
+const roundDisplay = document.getElementById('round');
+const firstNumberDisplay = document.getElementById('firstNumber');
+const betSection = document.getElementById('betSection');
+const guessSection = document.getElementById('guessSection');
 const resultDisplay = document.getElementById('result');
 
 document.addEventListener('DOMContentLoaded', () => {
     moneyDisplay.textContent = `Current money: $${money}`;
 });
 
-playButton.addEventListener('click', playGame);
+betButton.addEventListener('click', submitBet);
+guessButton.addEventListener('click', submitGuess);
 
-function playGame() {
+function submitBet() {
     const bet = parseInt(betInput.value);
-    const guess = guessInput.value.toUpperCase();
 
     if (isNaN(bet) || bet <= 0 || bet > money) {
         resultDisplay.textContent = 'Invalid bet. Please enter a valid bet.';
         return;
     }
 
+    betSection.style.display = 'none';
+    guessSection.style.display = 'block';
+
+    const firstNumber = Math.floor(Math.random() * 10) + 1;
+    firstNumberDisplay.textContent = `First number: ${firstNumber}`;
+}
+
+function submitGuess() {
+    const guess = guessInput.value.toUpperCase();
+    const bet = parseInt(betInput.value);
+    const firstNumber = parseInt(firstNumberDisplay.textContent.split(' ')[2]);
+
     if (guess !== 'H' && guess !== 'L') {
         resultDisplay.textContent = "Invalid input. Please enter 'H' for higher or 'L' for lower.";
         return;
     }
 
-    const firstNumber = Math.floor(Math.random() * 10) + 1;
     const secondNumber = Math.floor(Math.random() * 10) + 1;
 
     if ((secondNumber > firstNumber && guess === 'H') || (secondNumber < firstNumber && guess === 'L')) {
         money += bet;
-        resultDisplay.textContent = `First number: ${firstNumber}\nSecond number: ${secondNumber}\nCongratulations! You won!`;
+        resultDisplay.textContent = `Second number: ${secondNumber}\nCongratulations! You won!`;
     } else {
         money -= bet;
-        resultDisplay.textContent = `First number: ${firstNumber}\nSecond number: ${secondNumber}\nSorry, you lost.`;
+        resultDisplay.textContent = `Second number: ${secondNumber}\nSorry, you lost.`;
     }
 
     moneyDisplay.textContent = `Current money: $${money}`;
-
-    if (money <= 0) {
-        resultDisplay.textContent += '\nGame over. You lost all your money.';
-        playButton.disabled = true;
-    } else if (money >= 1000) {
-        resultDisplay.textContent += '\nCongratulations! You reached $1000 and won the game!';
-        playButton.disabled = true;
-    }
-}
+    round++;
