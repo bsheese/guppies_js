96765086e8e75f91037cf529966c46e72e37dd38
bsheese <bsheese@iwu.edu>
2023-05-05 23:00:17 -0500

    Update guppies.js

--- guppies.js
+++ guppies.js
@@ -1,38 +1,42 @@
 let money = 100;
 const betInput = document.getElementById('bet');
+const guessInput = document.getElementById('guess');
 const playButton = document.getElementById('play');
 const moneyDisplay = document.getElementById('money');
 const resultDisplay = document.getElementById('result');
 
 playButton.addEventListener('click', () => {
     const bet = parseInt(betInput.value);
+    const guess = guessInput.value.toUpperCase();
 
     if (isNaN(bet) || bet <= 0 || bet > money) {
         resultDisplay.textContent = 'Invalid bet. Please enter a valid bet.';
         return;
     }
 
-    const firstNumber = Math.floor(Math.random() * 10) + 1;
-    const higherOrLower = prompt(`First number: ${firstNumber}\nWill the second number be higher or lower? (H/L)`).toUpperCase();
+    if (guess !== 'H' && guess !== 'L') {
+        resultDisplay.textContent = "Invalid input. Please enter 'H' for higher or 'L' for lower.";
+        return;
+    }
 
+    const firstNumber = Math.floor(Math.random() * 10) + 1;
     const secondNumber = Math.floor(Math.random() * 10) + 1;
 
-    if ((secondNumber > firstNumber && higherOrLower === 'H') || (secondNumber < firstNumber && higherOrLower === 'L')) {
+    if ((secondNumber > firstNumber && guess === 'H') || (secondNumber < firstNumber && guess === 'L')) {
         money += bet;
-        resultDisplay.textContent = `Second number: ${secondNumber}\nCongratulations! You won!`;
+        resultDisplay.textContent = `First number: ${firstNumber}\nSecond number: ${secondNumber}\nCongratulations! You won!`;
     } else {
         money -= bet;
-        resultDisplay.textContent = `Second number: ${secondNumber}\nSorry, you lost.`;
+        resultDisplay.textContent = `First number: ${firstNumber}\nSecond number: ${secondNumber}\nSorry, you lost.`;
     }
 
     moneyDisplay.textContent = `Current money: $${money}`;
 
     if (money <= 0) {
-           resultDisplay.textContent += '\nGame over. You lost all your money.';
-           playButton.disabled = true;
-       } else if (money >= 1000) {
-           resultDisplay.textContent += '\nCongratulations! You reached $1000 and won the game!';
-           playButton.disabled = true;
-       }
-   });
-       
+        resultDisplay.textContent += '\nGame over. You lost all your money.';
+        playButton.disabled = true;
+    } else if (money >= 1000) {
+        resultDisplay.textContent += '\nCongratulations! You reached $1000 and won the game!';
+        playButton.disabled = true;
+    }
+});
